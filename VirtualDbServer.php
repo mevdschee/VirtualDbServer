3b8ed7c663ea27d9ad8959cb13d3accbc424511b
Maurits van der Schee <m.vanderschee@leaseweb.com>
2013-07-28 02:26:33 +0200

    fixed todo's in code

--- VirtualDbServer.php
+++ VirtualDbServer.php
@@ -12,27 +12,27 @@ class VirtualDbStatement implements Iterator {
   
   /*PDOStatement implements Traversable {
 
-   ok readonly string $queryString;
+   done readonly string $queryString;
       
-   ok public bool bindColumn ( mixed $column , mixed &$param [, int $type [, int $maxlen [, mixed $driverdata ]]] )
-   ok public bool bindParam ( mixed $parameter , mixed &$variable [, int $data_type = PDO::PARAM_STR [, int $length [, mixed $driver_options ]]] )
-   ok public bool bindValue ( mixed $parameter , mixed $value [, int $data_type = PDO::PARAM_STR ] )
-   ok public bool closeCursor ( void )
-   ok public int columnCount ( void )
-      public void debugDumpParams ( void )
-   ok public string errorCode ( void )
-   ok public array errorInfo ( void )
-   ok public bool execute ([ array $input_parameters ] )
-   ok public mixed fetch ([ int $fetch_style [, int $cursor_orientation = PDO::FETCH_ORI_NEXT [, int $cursor_offset = 0 ]]] )
-   ok public array fetchAll ([ int $fetch_style [, mixed $fetch_argument [, array $ctor_args = array() ]]] )
-   ok public string fetchColumn ([ int $column_number = 0 ] )
-   ok public mixed fetchObject ([ string $class_name = "stdClass" [, array $ctor_args ]] )
-   ok public mixed getAttribute ( int $attribute )
-   ok public array getColumnMeta ( int $column )
-      public bool nextRowset ( void )
-   ok public int rowCount ( void )
-   ok public bool setAttribute ( int $attribute , mixed $value )
-   ok public bool setFetchMode ( int $mode )
+   args public bool bindColumn ( mixed $column , mixed &$param [, int $type [, int $maxlen [, mixed $driverdata ]]] )
+   args public bool bindParam ( mixed $parameter , mixed &$variable [, int $data_type = PDO::PARAM_STR [, int $length [, mixed $driver_options ]]] )
+   args public bool bindValue ( mixed $parameter , mixed $value [, int $data_type = PDO::PARAM_STR ] )
+   done public bool closeCursor ( void )
+   done public int columnCount ( void )
+        public void debugDumpParams ( void )
+   done public string errorCode ( void )
+   done public array errorInfo ( void )
+   done public bool execute ([ array $input_parameters ] )
+   args public mixed fetch ([ int $fetch_style [, int $cursor_orientation = PDO::FETCH_ORI_NEXT [, int $cursor_offset = 0 ]]] )
+   done public array fetchAll ([ int $fetch_style [, mixed $fetch_argument [, array $ctor_args = array() ]]] )
+   done public string fetchColumn ([ int $column_number = 0 ] )
+   done public mixed fetchObject ([ string $class_name = "stdClass" [, array $ctor_args ]] )
+   done public mixed getAttribute ( int $attribute )
+   done public array getColumnMeta ( int $column )
+        public bool nextRowset ( void )
+   done public int rowCount ( void )
+   done public bool setAttribute ( int $attribute , mixed $value )
+   done public bool setFetchMode ( int $mode )
   }*/
   
   public $queryString;
@@ -291,21 +291,21 @@ class VirtualDbStatement implements Iterator {
 class VirtualDbServer
 {
   /* PDO {
-   ok public __construct ( string $dsn [, string $username [, string $password [, array $driver_options ]]] )
-      public bool beginTransaction ( void )
-      public bool commit ( void )
-   ok public mixed errorCode ( void )
-   ok public array errorInfo ( void )
-   ok public int exec ( string $statement )
-   ok public mixed getAttribute ( int $attribute )
-   ok public static array getAvailableDrivers ( void )
-      public bool inTransaction ( void )
-   ok public string lastInsertId ([ string $name = NULL ] )
-   ok public PDOStatement prepare ( string $statement [, array $driver_options = array() ] )
-   ok public PDOStatement query ( string $statement )
-   ok public string quote ( string $string [, int $parameter_type = PDO::PARAM_STR ] )
-      public bool rollBack ( void )
-   ok public bool setAttribute ( int $attribute , mixed $value )
+   done public __construct ( string $dsn [, string $username [, string $password [, array $driver_options ]]] )
+        public bool beginTransaction ( void )
+        public bool commit ( void )
+   done public mixed errorCode ( void )
+   done public array errorInfo ( void )
+   done public int exec ( string $statement )
+   done public mixed getAttribute ( int $attribute )
+   done public static array getAvailableDrivers ( void )
+        public bool inTransaction ( void )
+   done public string lastInsertId ([ string $name = NULL ] )
+   done public PDOStatement prepare ( string $statement [, array $driver_options = array() ] )
+   done public PDOStatement query ( string $statement )
+   args public string quote ( string $string [, int $parameter_type = PDO::PARAM_STR ] )
+        public bool rollBack ( void )
+   done public bool setAttribute ( int $attribute , mixed $value )
    }*/
   
   private $ch;
